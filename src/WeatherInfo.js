b71094f89b7a8cff3c4cce47c0bc35678abf6a72
corinamittelholzer <mittelholzer.corina@gmail.com>
2022-06-03 19:45:56 +0200

    fix weather forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,8 +5,8 @@ import WeatherForecast from "./WeatherForecast";
 
 export default function WeatherInfo(props) {
   return (
-    <div className=" WeatherInfo">
-      <div className="card mt-3 mb-3">
+    <div className="WeatherInfo">
+      <div className="card mt-3 mb-3 pb-4 pt-2">
         <div className="row">
           <div className="col-5">
             <div className="card-body info">
